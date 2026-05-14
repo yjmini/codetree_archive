d08c8c440b05dd1b79d595fe3e29baaa2cea0c98
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-14 10:58:43 +0900

    [Passed] 소수점 반올림하기 (9ms, 0MB)

---
+++ trail1/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    cout << fixed;
+    float a = 25.352;
+    cout.precision(1);
+    cout << a;
+    return 0;
+}
