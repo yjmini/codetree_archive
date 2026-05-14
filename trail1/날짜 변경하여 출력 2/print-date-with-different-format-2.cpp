3b163783715faba3d05598cc5fccb8bca17a3f54
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-15 00:25:29 +0900

    [Passed] 날짜 변경하여 출력 2 (9ms, 0MB)

---
+++ trail1/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int m, d, y;
+
+    cin >> m;
+    cin.get();
+    cin >> d;
+    cin.get();
+    cin >> y;
+    cin.get();
+
+    cout << y << "."<< m <<"."<<d;
+
+    return 0;
+}
