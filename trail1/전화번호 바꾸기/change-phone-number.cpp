498462da950c48c7c8856891f2617a8e1470c3ef
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-15 00:30:43 +0900

    [Passed] 전화번호 바꾸기 (8ms, 0MB)

---
+++ trail1/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int w, x, y;
+    cin >> w;
+    cin.get();
+    cin >> x;
+    cin.get();
+    cin >> y;
+    
+    cout << "010-" << y << "-"<< x;
+
+    return 0;
+}
