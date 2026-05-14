cc28ef57900908720583c863965b7bc836581654
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-15 00:27:36 +0900

    [Passed] 입력받아 출력 2 (8ms, 0MB)

---
+++ trail1/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int f, b;
+    cin >> f;
+    cin.get();
+    cin >> b;
+
+    cout << f<< b;
+
+    return 0;
+}
