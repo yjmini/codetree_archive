3185678142c6c1c4254ecd5f311132651ddc21de
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-15 00:23:40 +0900

    [Passed] 1시간 뒤 시간 출력 (8ms, 0MB)

---
+++ trail1/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int h, m;
+
+    cin >> h;
+    cin.get();
+    cin >> m;
+
+    cout << h+1 << ":"<<m;
+
+    return 0;
+}
