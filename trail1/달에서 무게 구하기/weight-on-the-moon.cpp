c16376d354e1d20184f28394d11b93d39cdbc0ea
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-14 10:58:03 +0900

    [Passed] 달에서 무게 구하기 (9ms, 0MB)

---
+++ trail1/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a= 13;
+    float b = 0.165;
+
+    cout << fixed;
+
+    cout.precision(6);
+
+    
+    cout << a << " * " << b << " = ";
+    cout << a*b;
+    return 0;
+}
