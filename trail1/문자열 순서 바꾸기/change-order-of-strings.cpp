f376ff7c94a01e486d33c274866327ecf1fa3f7e
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-14 21:32:46 +0900

    [Passed] 문자열 순서 바꾸기 (8ms, 0MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string T;
+    string S;
+
+    cin >> S >> T;
+
+    cout << T << '\n' << S;
+    
+    return 0;
+}
