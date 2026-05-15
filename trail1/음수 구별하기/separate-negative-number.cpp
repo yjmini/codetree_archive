f4dfe8ae59f814273a5cbf0d88996ed77276a9cb
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-15 11:26:00 +0900

    [Passed] 음수 구별하기 (8ms, 0MB)

---
+++ trail1/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+    cin >> N;
+
+    cout << N << '\n';
+    if (N < 0) {
+        cout << "minus";
+    }
+
+    return 0;
+}
