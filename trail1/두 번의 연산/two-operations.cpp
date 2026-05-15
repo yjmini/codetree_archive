97f60f632b4726181d6391be2b77ca05e1dd3711
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-15 17:29:33 +0900

    [Passed] 두 번의 연산 (8ms, 0MB)

---
+++ trail1/두 번의 연산/two-operations.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a;
+    cin >> a;
+
+    if (a % 2 == 1) {
+        a += 3;
+    }
+    if (a % 3 == 0) {
+        a /= 3;
+    }
+
+    cout << a;
+    
+    return 0;
+}
