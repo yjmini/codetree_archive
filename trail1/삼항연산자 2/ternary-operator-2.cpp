668d6b88e13bef8975b138f9106566ea681c6b84
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-15 17:23:18 +0900

    [Passed] 삼항연산자 2 (8ms, 0MB)

---
+++ trail1/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a;
+    cin >> a;
+
+    a == 1 ? cout << "t" : cout << "f";
+
+    return 0;
+}
