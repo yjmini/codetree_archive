43680d86a10126040a7f395b0a553c00a42d224f
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-17 12:07:35 +0900

    [Passed] 4번의 크기 비교 (8ms, 0MB)

---
+++ trail1/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a, b, c, d, e; 
+    cin >> a >> b >> c >> d >> e;
+
+    a > b ? cout << 1 << '\n' : cout << 0 << '\n';
+    a > c ? cout << 1 << '\n' : cout << 0 << '\n';
+    a > d ? cout << 1 << '\n' : cout << 0 << '\n';
+    a > e ? cout << 1 << '\n' : cout << 0 << '\n';
+
+    return 0;
+}
