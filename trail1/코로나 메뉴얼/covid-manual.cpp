91cc1071bb1d209b595da916d932b1e5b9b5da08
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-18 15:36:33 +0900

    [Passed] 코로나 메뉴얼 (2ms, 0MB)

---
+++ trail1/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    char symptom[3];
+    int temp[3];
+    int cnt;
+
+    for (int i = 0; i < 3; i++) {
+        cin >> symptom[i] >> temp[i];
+        
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (symptom[i] == 'Y' && temp[i] >= 37) {
+            cnt++;
+        }
+    }
+
+
+    if (cnt >= 2) {
+        cout << "E";
+    }
+    else {
+        cout << "N";
+    }
+
+
+
+    return 0;
+}
