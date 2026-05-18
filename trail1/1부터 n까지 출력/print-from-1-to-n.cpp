6bd64069ad933f2827a8e42930a9360d58bef7ec
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-18 15:42:32 +0900

    [Passed] 1부터 n까지 출력 (8ms, 0MB)

---
+++ trail1/1부터 n까지 출력/print-from-1-to-n.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cout << i+1 << " ";
+    }
+    return 0;
+}
