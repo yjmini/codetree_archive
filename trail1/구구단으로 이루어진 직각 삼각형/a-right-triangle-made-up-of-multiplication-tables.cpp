82fea8cda53d095e6cc37d98358eb5b83caceab4
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-23 17:31:50 +0900

    [Passed] 구구단으로 이루어진 직각 삼각형 (2ms, 0MB)

---
+++ trail1/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i + 1; j++) {
+            cout << i << " * " << j << " = " << i * j;
+            if (j != n - i + 1) {
+                cout << " / ";
+            }
+            else {
+                cout << '\n';
+            }
+        }
+    }
+    return 0;
+}
