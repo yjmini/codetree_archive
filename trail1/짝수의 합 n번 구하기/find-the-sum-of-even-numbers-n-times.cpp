97dae3ec494eb079e3dc75e173d4baa414384ace
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-23 21:58:16 +0900

    [Passed] 짝수의 합 n번 구하기 (1ms, 0MB)

---
+++ trail1/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        int sum = 0;
+
+        for (int j = a; j <= b; j++) {
+            if (j % 2 == 0) {
+                sum += j;
+            }
+        }
+        cout << sum << '\n';
+    }
+    return 0;
+}
