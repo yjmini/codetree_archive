ed7a6b16833e064215e7273cd15527146601b484
yjmini <74178991+yjmini@users.noreply.github.com>
2026-05-30 22:25:32 +0900

    [Passed] 차례로 출력 (2ms, 0MB)

---
+++ trail0/차례로 출력/print-in-order.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    for (int i = 5; i <= 17; i++) {
+        cout << i << " ";
+    }
+    return 0;
+}
