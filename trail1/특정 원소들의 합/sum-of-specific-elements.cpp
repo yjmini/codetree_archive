8d115aa82d2677396c17e0b0a63d98ed3095693a
yjmini <74178991+yjmini@users.noreply.github.com>
2026-06-03 22:58:54 +0900

    [Passed] 특정 원소들의 합 (2ms, 0MB)

---
+++ trail1/특정 원소들의 합/sum-of-specific-elements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int arr[4][4] = {0};
+    int sum = 0;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j <= i; j++) {
+            sum += arr[i][j];        
+        }
+    }
+
+    cout << sum;
+
+    return 0;
+}
