11a0ec80178ddce13615e2111056edc235c231d3
yjmini <74178991+yjmini@users.noreply.github.com>
2026-06-07 23:10:49 +0900

    [Passed] 문자열 추가하기 (2ms, 0MB)

---
+++ trail1/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string s1; 
+    cin >> s1;
+
+    cout << s1 + "Hello";
+    
+    return 0;
+}
