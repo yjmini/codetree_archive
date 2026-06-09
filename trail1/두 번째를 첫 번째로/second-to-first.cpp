71a9189e94b0046fe64557d0d04379425ac8df26
yjmini <74178991+yjmini@users.noreply.github.com>
2026-06-09 17:33:35 +0900

    [Passed] 두 번째를 첫 번째로 (2ms, 0MB)

---
+++ trail1/두 번째를 첫 번째로/second-to-first.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string s1;
+    cin >> s1;
+    char c1;
+    c1 = s1[0];
+    char c2;
+    c2 = s1[1];
+
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1[i] == c2) {
+            s1[i] = c1;
+        }
+    }
+
+    cout << s1;
+    
+    return 0;
+}
