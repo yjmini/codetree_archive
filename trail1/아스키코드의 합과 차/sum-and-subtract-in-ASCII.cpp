ed228b57e08afdc72ad0a5849b141503022a49a1
yjmini <74178991+yjmini@users.noreply.github.com>
2026-06-11 22:51:11 +0900

    [Passed] 아스키코드의 합과 차 (2ms, 0MB)

---
+++ trail1/아스키코드의 합과 차/sum-and-subtract-in-ASCII.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char a, b;
+    cin >> a >> b;
+
+    cout << int(a+b) << " " << abs(int(a-b));
+    return 0;
+}
