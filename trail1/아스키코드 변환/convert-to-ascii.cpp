7c0f81176432fea68f3f00183537d1369d77442f
yjmini <74178991+yjmini@users.noreply.github.com>
2026-06-15 10:49:58 +0900

    [Passed] 아스키코드 변환 (2ms, 0MB)

---
+++ trail1/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    
+    char a;
+    int b;
+    cin >> a >> b;
+
+    cout << int(a) << " " << char(b);
+    return 0;
+}
