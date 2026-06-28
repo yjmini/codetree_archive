45d48f74c7cf94c5dc4c21d2b1f2e48c0e2eb870
yjmini <74178991+yjmini@users.noreply.github.com>
2026-06-28 23:24:28 +0900

    [Passed] 친한 점 (73ms, 3MB)

---
+++ trail5/친한 점/frendly-point.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <set>
+#include <utility>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    set<pair<int, int>> s;
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        s.insert(make_pair(x, y));
+    }
+
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        auto it = s.lower_bound(make_pair(x, y));
+        if (it != s.end()) {
+            cout << it->first << " " << it->second << '\n';
+        }
+        else {
+            cout << "-1 -1\n";
+        }
+    }
+    return 0;
+}
